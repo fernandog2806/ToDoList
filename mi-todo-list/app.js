0bef6afbeab8da4a861f049914f0e52b265c5154
fernandog2806 <fernando.gonzalez@estudiantes.unahur.edu.ar>
2026-03-23 16:28:11 -0300

    feat: agenda completa con carrusel de fechas, horarios opcionales y diseño responsive

--- mi-todo-list/app.js
+++ mi-todo-list/app.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+// app.listen(3000, () => {
+//     console.log('Servidor corriendo en http://localhost:3000');
+// });
+
 if (process.env.NODE_ENV !== 'production') {
     app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
 }
